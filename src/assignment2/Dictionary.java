a60dfa729102a6c757f18fe273e96ef97da9f45f
qlows <qlows@protonmail.com>
2022-03-25 18:58:43 -0400

    added menu

---
+++ src/assignment2/Dictionary.java
@@ -0,0 +1,13 @@
+
+package assignment2;
+
+/**
+ *
+ * @author UMIT KILINC 101232721
+ * @author RAVKEERAT SINGH 
+ */
+public class Dictionary {
+    public boolean add(String word, String meaning){
+        
+    }
+}
